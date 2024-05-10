a088d5ac85ff1c614d0ec680a88cf80c14384d59
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-05-10 16:18:17 +0530

    Update QRCode.js

--- QRCode.js
+++ QRCode.js
@@ -20,27 +20,3 @@ QRCode();
 
 
 
-// const QrCode=require('qrcode');
-// const path=require('path');
-// // QrCode.toString('data goes here',(err,data)=>{
-// //     if(err) throw err;
-// //     console.log(data);
-// // })
-
-// QrCode.toFile(path.join(__dirname,'qrCode.png'),'random data here',(err)=>{
-//     if(err) throw err;
-//     console.log('QR code generated and saved as qrCode.png');
-// })
-
-// const QrCode = require('qrcode');
-// const path = require('path');
-// const readlineSync = require('readline-sync');
-
-// // Prompt the user to enter data for the QR code
-// const data = readlineSync.question('Enter data for QR code: ');
-
-// // Generate and save the QR code as an image file
-// QrCode.toFile(path.join(__dirname, 'qrCode.png'), data, (err) => {
-//     if (err) throw err;
-//     console.log('QR code generated and saved as qrCode.png');
-// });
